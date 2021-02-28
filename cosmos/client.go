6a9b8eb515536fb77aed9eb12e7fdcaa38885ac2
tpkeeper <tpkeeper.me@gmail.com>
2021-02-28 12:35:15 +0800

    cosmos: add multisig

--- cosmos/client.go
+++ cosmos/client.go
@@ -1,12 +1,17 @@
 package cosmos
 
 import (
+	"errors"
 	"fmt"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	clientTx "github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	kMultiSig "github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
+	"github.com/cosmos/cosmos-sdk/crypto/types/multisig"
 	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	xAuthClient "github.com/cosmos/cosmos-sdk/x/auth/client"
 	xAuthTypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	xBankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/spf13/cobra"
@@ -16,9 +21,8 @@ import (
 
 //cosmos client
 type Client struct {
-	clientCtx   client.Context
-	rpcClient   rpcClient.Client
-	fromKeyInfo keyring.Info
+	clientCtx client.Context
+	rpcClient rpcClient.Client
 }
 
 func NewClient(rpcClient rpcClient.Client, k keyring.Keyring, chainId, fromKeyName string) *Client {
@@ -44,14 +48,135 @@ func NewClient(rpcClient rpcClient.Client, k keyring.Keyring, chainId, fromKeyNa
 		WithKeyring(k)
 
 	return &Client{
-		clientCtx:   initClientCtx,
-		rpcClient:   rpcClient,
-		fromKeyInfo: info,
+		clientCtx: initClientCtx,
+		rpcClient: rpcClient,
 	}
+}
+
+//update from key
+func (c *Client) SetFromKey(fromKey string) error {
+	info, err := c.clientCtx.Keyring.Key(fromKey)
+	if err != nil {
+		return fmt.Errorf("keyring get address from fromKeyname err: %s", err)
+	}
+
+	c.clientCtx = c.clientCtx.WithFromName(fromKey).WithFromAddress(info.GetAddress())
+	return nil
 }
 
 func (c *Client) SendTo(toAddr types.AccAddress, amount types.Coins) error {
 	msg := xBankTypes.NewMsgSend(c.clientCtx.GetFromAddress(), toAddr, amount)
 	cmd := cobra.Command{}
 	return clientTx.GenerateOrBroadcastTxCLI(c.clientCtx, cmd.Flags(), msg)
+}
+
+func (c *Client) GenRawTx(toAddr types.AccAddress, amount types.Coins) ([]byte, error) {
+	account, err := c.clientCtx.AccountRetriever.GetAccount(c.clientCtx, c.clientCtx.GetFromAddress())
+	if err != nil {
+		return nil, err
+	}
+
+	msg := xBankTypes.NewMsgSend(c.clientCtx.GetFromAddress(), toAddr, amount)
+
+	cmd := cobra.Command{}
+	txf := clientTx.NewFactoryCLI(c.clientCtx, cmd.Flags())
+	txf = txf.WithSequence(account.GetSequence()).
+		WithAccountNumber(account.GetAccountNumber()).
+		WithSignMode(signing.SignMode_SIGN_MODE_LEGACY_AMINO_JSON) //multi sig need this mod
+
+	txBuilderRaw, err := clientTx.BuildUnsignedTx(txf, msg)
+	if err != nil {
+		return nil, err
+	}
+	return c.clientCtx.TxConfig.TxJSONEncoder()(txBuilderRaw.GetTx())
+}
+
+func (c *Client) SignRawTx(rawTx []byte, fromKey string) (signature []byte, err error) {
+	account, err := c.clientCtx.AccountRetriever.GetAccount(c.clientCtx, c.clientCtx.GetFromAddress())
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := cobra.Command{}
+	txf := clientTx.NewFactoryCLI(c.clientCtx, cmd.Flags())
+	txf = txf.WithSequence(account.GetSequence()).
+		WithAccountNumber(account.GetAccountNumber()).
+		WithSignMode(signing.SignMode_SIGN_MODE_LEGACY_AMINO_JSON) //multi sig need this mod
+
+	tx, err := c.clientCtx.TxConfig.TxJSONDecoder()(rawTx)
+	if err != nil {
+		return nil, err
+	}
+	txBuilder, err := c.clientCtx.TxConfig.WrapTxBuilder(tx)
+	if err != nil {
+		return nil, err
+	}
+	xAuthClient.SignTxWithSignerAddress(txf, c.clientCtx, c.clientCtx.GetFromAddress(), fromKey, txBuilder, true, false)
+
+	return marshalSignatureJSON(c.clientCtx.TxConfig, txBuilder, true)
+}
+
+func (c *Client) CreateMultiSigTx(rawTx []byte, signatures [][]byte) (txBts []byte, err error) {
+	accountMultiSign, err := c.clientCtx.AccountRetriever.GetAccount(c.clientCtx, c.clientCtx.GetFromAddress())
+	if err != nil {
+		return nil, err
+	}
+	legacyPubkey := accountMultiSign.GetPubKey().(*kMultiSig.LegacyAminoPubKey)
+
+	willUseSigs := make([]signing.SignatureV2, 0)
+	for _, s := range signatures {
+		ss, err := c.clientCtx.TxConfig.UnmarshalSignatureJSON(s)
+		if err != nil {
+			return nil, err
+		}
+		willUseSigs = append(willUseSigs, ss...)
+	}
+
+	multiSigData := multisig.NewMultisig(len(legacyPubkey.PubKeys))
+	for _, sig := range willUseSigs {
+		if err := multisig.AddSignatureV2(multiSigData, sig, legacyPubkey.GetPubKeys()); err != nil {
+			return nil, err
+		}
+	}
+
+	sigV2 := signing.SignatureV2{
+		PubKey:   legacyPubkey,
+		Data:     multiSigData,
+		Sequence: accountMultiSign.GetSequence(),
+	}
+
+	tx, err := c.clientCtx.TxConfig.TxJSONDecoder()(rawTx)
+	if err != nil {
+		return nil, err
+	}
+	txBuilder, err := c.clientCtx.TxConfig.WrapTxBuilder(tx)
+	if err != nil {
+		return nil, err
+	}
+	txBuilder.SetSignatures(sigV2)
+	return c.clientCtx.TxConfig.TxEncoder()(txBuilder.GetTx())
+}
+
+func (c *Client) BroadcastTx(tx []byte) error {
+	res, err := c.clientCtx.BroadcastTx(tx)
+	if err != nil {
+		return err
+	}
+	if res.Code != 0 {
+		return errors.New(fmt.Sprintf("Boradcast err with res.code: %d", res.Code))
+	}
+	return nil
+}
+
+func marshalSignatureJSON(txConfig client.TxConfig, txBldr client.TxBuilder, signatureOnly bool) ([]byte, error) {
+	parsedTx := txBldr.GetTx()
+	if signatureOnly {
+		sigs, err := parsedTx.GetSignaturesV2()
+		if err != nil {
+			return nil, err
+		}
+		return txConfig.MarshalSignatureJSON(sigs)
+	}
+
+	return txConfig.TxJSONEncoder()(parsedTx)
 }
--- cosmos/client_test.go
+++ cosmos/client_test.go
@@ -12,13 +12,14 @@ import (
 
 var client *cosmos.Client
 var addrMultiSig1, _ = types.AccAddressFromBech32("cosmos1ak3nrcmm7e4j8y7ycfc78pxl4g4lehf43vw6wu")
+var addrReceive, _ = types.AccAddressFromBech32("cosmos1cgs647rewxyzh5wu4e606kk7qyuj5f8hk20rgf")
 
 func init() {
 	rpcClient, err := rpcHttp.New("http://127.0.0.1:26657", "/websocket")
 	if err != nil {
 		panic(err)
 	}
-	key, err := keyring.New(types.KeyringServiceName(), keyring.BackendFile, "/Users/tpkeeper/.gaia",strings.NewReader("tpkeeper\n"))
+	key, err := keyring.New(types.KeyringServiceName(), keyring.BackendFile, "/Users/tpkeeper/.gaia", strings.NewReader("tpkeeper\n"))
 	if err != nil {
 		panic(err)
 	}
@@ -28,5 +29,42 @@ func init() {
 
 func TestClient_SendTo(t *testing.T) {
 	err := client.SendTo(addrMultiSig1, types.NewCoins(types.NewInt64Coin("stake", 100)))
+	assert.NoError(t, err)
+}
+
+func TestClient_GenRawTx(t *testing.T) {
+	err := client.SetFromKey("multiSign1")
+	assert.NoError(t, err)
+	rawTx, err := client.GenRawTx(addrReceive, types.NewCoins(types.NewInt64Coin("stake", 10)))
+	assert.NoError(t, err)
+	t.Log(string(rawTx))
+}
+
+func TestClient_SignRawTx(t *testing.T) {
+	err := client.SetFromKey("multiSign1")
+	assert.NoError(t, err)
+	rawTx, err := client.GenRawTx(addrReceive, types.NewCoins(types.NewInt64Coin("stake", 10)))
+	assert.NoError(t, err)
+
+	signature, err := client.SignRawTx(rawTx, "key1")
+	assert.NoError(t, err)
+	t.Log(string(signature))
+}
+
+func TestClient_CreateMultiSigTx(t *testing.T) {
+	err := client.SetFromKey("multiSign1")
+	assert.NoError(t, err)
+	rawTx, err := client.GenRawTx(addrReceive, types.NewCoins(types.NewInt64Coin("stake", 10)))
+	assert.NoError(t, err)
+
+	signature1, err := client.SignRawTx(rawTx, "key1")
+	assert.NoError(t, err)
+	signature2, err := client.SignRawTx(rawTx, "key2")
+	assert.NoError(t, err)
+
+	tx, err := client.CreateMultiSigTx(rawTx, [][]byte{signature1, signature2})
+	assert.NoError(t, err)
+
+	err = client.BroadcastTx(tx)
 	assert.NoError(t, err)
 }
