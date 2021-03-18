b358e369c39a0a74c50584bbe0427beda653cfca
tpkeeper <tpkeeper.me@gmail.com>
2021-03-18 21:07:19 +0800

    cosmos:add erapoolUpdated

--- chains/cosmos/connection.go
+++ chains/cosmos/connection.go
@@ -1,12 +1,14 @@
 package cosmos
 
 import (
+	"bytes"
 	"encoding/hex"
 	"errors"
 	"fmt"
 	"github.com/ChainSafe/log15"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"github.com/cosmos/cosmos-sdk/types"
+	xBankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stafiprotocol/rtoken-relay/core"
 	"github.com/stafiprotocol/rtoken-relay/shared/cosmos"
 	"github.com/stafiprotocol/rtoken-relay/shared/cosmos/rpc"
@@ -74,4 +76,76 @@ func NewConnection(cfg *core.ChainConfig, log log15.Logger, stop <-chan int) (*C
 		poolKeys:   keys,
 		subClients: subClients,
 	}, nil
+}
+
+func (fc *Connection) TransferVerify(r *core.BondRecord) (core.BondReason, error) {
+	if len(fc.subClients) == 0 || len(fc.poolKeys) == 0 {
+		return "", fmt.Errorf("no subClient")
+	}
+	hashStr := hex.EncodeToString(r.Txhash)
+	client := fc.subClients[fc.poolKeys[0]]
+
+	//check tx hash
+	res, err := client.GetRpcClient().QueryTxByHash(hashStr)
+	if err != nil {
+		return core.TxhashUnmatch, err
+	}
+	if res.Empty() {
+		return core.TxhashUnmatch, nil
+	}
+
+	//check block hash
+	blockRes, err := client.GetRpcClient().QueryBlock(res.Height)
+	if err != nil {
+		return core.BlockhashUnmatch, err
+	}
+	if !bytes.Equal(blockRes.BlockID.Hash, r.Blockhash) {
+		return core.BlockhashUnmatch, nil
+	}
+
+	//check amount and pool
+	amountIsMatch := false
+	poolIsMatch := false
+	fromAddressStr := ""
+	msgs := res.GetTx().GetMsgs()
+	for i, _ := range msgs {
+		if msgs[i].Type() == xBankTypes.TypeMsgSend {
+			sendMsg, ok := msgs[i].(*xBankTypes.MsgSend)
+			if ok {
+				toAddr, err := types.AccAddressFromBech32(sendMsg.ToAddress)
+				if err == nil {
+					//amount and pool address must in one message
+					if bytes.Equal(toAddr.Bytes(), r.Pool) &&
+						sendMsg.Amount.AmountOf(client.GetRpcClient().GetDenom()).Equal(types.NewIntFromBigInt(r.Amount.Int)) {
+						poolIsMatch = true
+						amountIsMatch = true
+						fromAddressStr = sendMsg.FromAddress
+					}
+				}
+
+			}
+
+		}
+	}
+	if !amountIsMatch {
+		return core.AmountUnmatch, nil
+	}
+	if !poolIsMatch {
+		return core.PoolUnmatch, nil
+	}
+
+	//check pubkey
+	fromAddress, err := types.AccAddressFromBech32(fromAddressStr)
+	if err != nil {
+		return core.PubkeyUnmatch, err
+	}
+	accountRes, err := client.GetRpcClient().QueryAccount(fromAddress)
+	if err != nil {
+		return core.PubkeyUnmatch, err
+	}
+
+	if !bytes.Equal(accountRes.GetPubKey().Bytes(), r.Pubkey) {
+		return core.PubkeyUnmatch, nil
+	}
+	return core.Pass, nil
 }
--- chains/cosmos/writer.go
+++ chains/cosmos/writer.go
@@ -1,6 +1,7 @@
 package cosmos
 
 import (
+	"fmt"
 	"github.com/ChainSafe/log15"
 	"github.com/stafiprotocol/rtoken-relay/chains"
 	"github.com/stafiprotocol/rtoken-relay/core"
@@ -35,9 +36,9 @@ func (w *writer) setRouter(r chains.Router) {
 func (w *writer) ResolveMessage(m *core.Message) bool {
 	switch m.Reason {
 	case core.LiquidityBond:
-	case core.LiquidityBondResult:
-	case core.NewEra:
+		return w.processLiquidityBond(m)
 	case core.EraPoolUpdated:
+		return w.processEraPoolUpdated(m)
 	case core.NewMultisig:
 	case core.MultisigExecuted:
 		return true
@@ -45,5 +46,54 @@ func (w *writer) ResolveMessage(m *core.Message) bool {
 		w.log.Warn("message reason unsupported", "reason", m.Reason)
 		return false
 	}
+	return true
+}
+
+func (w *writer) processLiquidityBond(m *core.Message) bool {
+	flow, ok := m.Content.(*core.BondFlow)
+	if !ok {
+		w.printContentError(m)
+		return false
+	}
+
+	if flow.Reason != core.BondReasonDefault {
+		w.log.Error("processLiquidityBond receive a message of which reason is not default", "bondId", flow.Key.BondId.Hex(), "reason", flow.Reason)
+		return false
+	}
+
+	bondReason, err := w.conn.TransferVerify(flow.Record)
+	if err != nil {
+		w.log.Error("TransferVerify error", "err", err, "bondId", flow.Key.BondId.Hex())
+		return false
+	}
+
+	flow.Reason = bondReason
+
+	result := &core.Message{Source: m.Destination, Destination: m.Source, Reason: core.LiquidityBondResult, Content: flow}
+	return w.submitMessage(result)
+}
+
+func (w *writer) processEraPoolUpdated(m *core.Message) bool {
+	flow, ok := m.Content.(*core.EraPoolUpdatedFlow)
+	if !ok {
+		w.printContentError(m)
+		return false
+	}
+	fmt.Sprintf("processEraPoolUpdated %v", flow)
+	return true
+}
+
+func (w *writer) printContentError(m *core.Message) {
+	w.log.Error("msg resolve failed", "source", m.Source, "dest", m.Destination, "reason", m.Reason)
+}
+
+// submitMessage inserts the chainId into the msg and sends it to the router
+func (w *writer) submitMessage(m *core.Message) bool {
+	err := w.router.Send(m)
+	if err != nil {
+		w.log.Error("failed to process event", "err", err)
+		return false
+	}
+
 	return true
 }
