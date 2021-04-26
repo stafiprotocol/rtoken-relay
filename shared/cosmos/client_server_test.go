6777b01001380d6a7658fa72ac622c02418b81a5
tpkeeper <tpkeeper.me@gmail.com>
2021-04-26 22:23:35 +0800

    fix endpoint

--- shared/cosmos/client_server_test.go
+++ shared/cosmos/client_server_test.go
@@ -16,14 +16,16 @@ var addrMultiKey1, _ = types.AccAddressFromBech32("cosmos12yprrdprzat35zhqxe2fcn
 var adrValidatorTestnetTecos, _ = types.ValAddressFromBech32("cosmosvaloper1p7e37nztj62mmra8xhgqde7sql3llhhu6hvcx8")
 var adrValidatorEverStake, _ = types.ValAddressFromBech32("cosmosvaloper1tflk30mq5vgqjdly92kkhhq3raev2hnz6eete3")
 
-
 func init() {
 	key, err := keyring.New(types.KeyringServiceName(), keyring.BackendFile, "/home/stafi/ratom/keys", strings.NewReader("12345678\n"))
 	if err != nil {
 		panic(err)
 	}
 
-	client, _ = rpc.NewClient(key, "stargate-final", "recipient", "0.04umuon", "umuon", "https://testcosmosrpc.wetez.io")
+	client, err = rpc.NewClient(key, "stargate-final", "recipient", "0.04umuon", "umuon", "https://testcosmosrpc.wetez.io:443")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func TestClient_SendTo(t *testing.T) {
@@ -34,7 +36,9 @@ func TestClient_SendTo(t *testing.T) {
 func TestClient_GenMultiSigRawDelegateTx(t *testing.T) {
 	err := client.SetFromName("multiSign1")
 	assert.NoError(t, err)
-	rawTx, err := client.GenMultiSigRawDelegateTx(addrMultiKey1, []types.ValAddress{adrValidatorTestnetTecos, adrValidatorEverStake}, types.NewCoin(client.GetDenom(), types.NewInt(10000000)))
+	rawTx, err := client.GenMultiSigRawDelegateTx(addrMultiKey1, []types.ValAddress{adrValidatorTestnetTecos, adrValidatorEverStake},
+		types.NewCoin(client.GetDenom(), types.NewInt(10000000)))
+
 	assert.NoError(t, err)
 
 	signature1, err := client.SignMultiSigRawTx(rawTx, "multisubkey1")
