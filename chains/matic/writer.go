a5ca13759ae0934e7b8e969851ef07a7add3e721
fengwenjun <kael@fengwenjun.com>
2021-07-11 22:31:20 +0800

    no nonce to withdraw will still informChain

--- chains/matic/writer.go
+++ chains/matic/writer.go
@@ -353,10 +353,9 @@ func (w *writer) processActiveReported(m *core.Message) bool {
 	}
 
 	if nonce.Uint64() == 0 {
-		err = fmt.Errorf("need to withdraw, but no nonce is withdrawable, share: %s, pool: %s", shareAddr.Hex(), poolAddr.Hex())
-		w.log.Error(err.Error())
-		w.sysErr <- err
-		return false
+		mef.OpaqueCalls = []*submodel.MultiOpaqueCall{{CallHash: txHash.Hex()}}
+		w.log.Info("find no nonce to withdraw", "share", shareAddr, "pool", poolAddr)
+		return w.informChain(m.Destination, m.Source, mef)
 	}
 
 	tx, err := w.conn.WithdrawCall(shareAddr, common.BytesToAddress(snap.Pool), nonce)
