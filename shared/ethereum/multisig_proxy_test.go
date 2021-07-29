f1b56689a759a1806cbe82a111d1f7ebff33dd1f
fengwenjun <kael@fengwenjun.com>
2021-07-29 21:56:58 +0800

    multisig_proxy_test

--- shared/ethereum/multisig_proxy_test.go
+++ shared/ethereum/multisig_proxy_test.go
@@ -1,13 +1,20 @@
 package ethereum
 
 import (
+	"math/big"
+	"os"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stafiprotocol/chainbridge/utils/crypto/secp256k1"
+	"github.com/stafiprotocol/chainbridge/utils/keystore"
 	"github.com/stafiprotocol/rtoken-relay/bindings/MaticToken"
 	"github.com/stafiprotocol/rtoken-relay/bindings/Multisig"
 	"github.com/stafiprotocol/rtoken-relay/bindings/ProxyAdmin"
 	"github.com/stafiprotocol/rtoken-relay/models/ethmodel"
+	"github.com/stafiprotocol/rtoken-relay/utils"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -59,89 +66,89 @@ func TestMultisigProxyStorage(t *testing.T) {
 	assert.Equal(t, state, uint8(ethmodel.HashStateSuccess))
 }
 
-//func TestMultisigProxyUpgrade(t *testing.T) {
-//	password := "123456"
-//	os.Setenv(keystore.EnvPassword, password)
-//
-//	admin := common.HexToAddress("0xBd39f5936969828eD9315220659cD11129071814")
-//	kpI, err := keystore.KeypairFromAddress(admin.Hex(), keystore.EthChain, keystorePath, false)
-//	if err != nil {
-//		panic(err)
-//	}
-//	kp, _ := kpI.(*secp256k1.Keypair)
-//
-//	client := NewClient(goerliEndPoint, kp, testLogger, big.NewInt(0), big.NewInt(0))
-//	err = client.Connect()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	proxyAdmin, err := ProxyAdmin.NewProxyAdmin(goerliProxyAdmin, client.Client())
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	newImpl := common.HexToAddress("0x33b520a2eB16e08C4110564b70D0F5e57a1Bcf04")
-//
-//	err = client.LockAndUpdateOpts(big.NewInt(0))
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	tx, err := proxyAdmin.Upgrade(client.Opts(), goerliMultisigProxy, newImpl)
-//	client.UnlockOpts()
-//
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	t.Log("Upgrade txHash", tx.Hash())
-//}
+func TestMultisigProxyUpgrade(t *testing.T) {
+	password := "123456"
+	os.Setenv(keystore.EnvPassword, password)
 
-//func TestMultisigProxyInitialize(t *testing.T) {
-//	password := "123456"
-//	os.Setenv(keystore.EnvPassword, password)
-//
-//	kpI, err := keystore.KeypairFromAddress(owner.Hex(), keystore.EthChain, keystorePath, false)
-//	if err != nil {
-//		panic(err)
-//	}
-//	kp, _ := kpI.(*secp256k1.Keypair)
-//
-//	client := NewClient(goerliEndPoint, kp, testLogger, big.NewInt(0), big.NewInt(0))
-//	err = client.Connect()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	multisig, err := Multisig.NewMultisig(goerliMultisigProxy, client.Client())
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	th, err := multisig.GetThreshold(nil)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	t.Log("threshold", th)
-//
-//	//owners := []common.Address{common.HexToAddress("0xBca9567A9e8D5F6F58C419d32aF6190F74C880e6"), common.HexToAddress("0xBd39f5936969828eD9315220659cD11129071814")}
-//	//
-//	//err = client.LockAndUpdateOpts()
-//	//if err != nil {
-//	//	t.Fatal(err)
-//	//}
-//	//tx, err := multisig.Initialize(client.opts, owners, big.NewInt(2))
-//	//client.UnlockOpts()
-//	//if err != nil {
-//	//	t.Fatal(err)
-//	//}
-//	//t.Log("initialize txHash", tx.Hash())
-//	//
-//	//th, err = multisig.GetThreshold(nil)
-//	//if err != nil {
-//	//	t.Fatal(err)
-//	//}
-//	//t.Log("threshold after initialize", th)
-//}
+	admin := common.HexToAddress("0xBd39f5936969828eD9315220659cD11129071814")
+	kpI, err := keystore.KeypairFromAddress(admin.Hex(), keystore.EthChain, keystorePath, false)
+	if err != nil {
+		panic(err)
+	}
+	kp, _ := kpI.(*secp256k1.Keypair)
+
+	client := NewClient(goerliEndPoint, kp, testLogger, big.NewInt(0), big.NewInt(0))
+	err = client.Connect()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	proxyAdmin, err := ProxyAdmin.NewProxyAdmin(goerliProxyAdmin, client.Client())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newImpl := common.HexToAddress("0xF10EBfe15cC878afdE72d7777a1a1e8689DD2c52")
+
+	err = client.LockAndUpdateOpts(big.NewInt(0))
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx, err := proxyAdmin.Upgrade(client.Opts(), goerliMultisigProxy, newImpl)
+	client.UnlockOpts()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Upgrade txHash", tx.Hash())
+}
+
+func TestMultisigProxyInitialize(t *testing.T) {
+	password := "123456"
+	os.Setenv(keystore.EnvPassword, password)
+
+	kpI, err := keystore.KeypairFromAddress(owner.Hex(), keystore.EthChain, keystorePath, false)
+	if err != nil {
+		panic(err)
+	}
+	kp, _ := kpI.(*secp256k1.Keypair)
+
+	client := NewClient(goerliEndPoint, kp, testLogger, big.NewInt(0), big.NewInt(0))
+	err = client.Connect()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	multisig, err := Multisig.NewMultisig(goerliMultisigProxy, client.Client())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	th, err := multisig.GetThreshold(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("threshold", th)
+
+	//owners := []common.Address{common.HexToAddress("0xBca9567A9e8D5F6F58C419d32aF6190F74C880e6"), common.HexToAddress("0xBd39f5936969828eD9315220659cD11129071814")}
+	//
+	//err = client.LockAndUpdateOpts()
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//tx, err := multisig.Initialize(client.opts, owners, big.NewInt(2))
+	//client.UnlockOpts()
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//t.Log("initialize txHash", tx.Hash())
+	//
+	//th, err = multisig.GetThreshold(nil)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//t.Log("threshold after initialize", th)
+}
 
 func TestErc20Balance(t *testing.T) {
 	client := NewGoerliClient()
@@ -157,81 +164,83 @@ func TestErc20Balance(t *testing.T) {
 	t.Log("bal", bal)
 }
 
-//func TestProxyTransferErc20(t *testing.T) {
-//	password := "123456"
-//	os.Setenv(keystore.EnvPassword, password)
-//
-//	owners := []common.Address{common.HexToAddress("0xBd39f5936969828eD9315220659cD11129071814"), common.HexToAddress("0xBca9567A9e8D5F6F58C419d32aF6190F74C880e6")}
-//	keys := make([]*secp256k1.Keypair, 0)
-//
-//	for _, own := range owners {
-//		kpI, err := keystore.KeypairFromAddress(own.Hex(), keystore.EthChain, keystorePath, false)
-//		if err != nil {
-//			panic(err)
-//		}
-//		kp, _ := kpI.(*secp256k1.Keypair)
-//
-//		keys = append(keys, kp)
-//	}
-//
-//	client := NewClient(goerliEndPoint, keys[0], testLogger, big.NewInt(0), big.NewInt(0))
-//	err := client.Connect()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	cd, _ := maticTokenAbi.Pack("transfer", receiver1, big.NewInt(0).Mul(big.NewInt(10000000000000000), big.NewInt(10)))
-//	mt := &ethmodel.MultiTransaction{
-//		To:        goerliRFis,
-//		Value:     big.NewInt(0),
-//		CallData:  cd,
-//		Operation: ethmodel.Call,
-//		SafeTxGas: big.NewInt(100000),
-//		TotalGas: big.NewInt(100000),
-//	}
-//	txhash := common.HexToHash("0x1ed668ca5c97408167f046131a37b4ef10ccbd621dabf920eefddaa62fe77e1b")
-//	msg := mt.MessageToSign(txhash, goerliMultisigProxy)
-//	t.Log("msg", hexutil.Encode(msg[:]))
-//
-//	multi, err := Multisig.NewMultisig(goerliMultisigProxy, client.Client())
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	sigs := make([][]byte, 0)
-//	for _, key := range keys {
-//		signature, err := crypto.Sign(msg[:], key.PrivateKey())
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		sigs = append(sigs, signature)
-//	}
-//
-//	vs, rs, ss := utils.DecomposeSignature(sigs)
-//	err = client.LockAndUpdateOpts(mt.TotalGas)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	tx, err := multi.ExecTransaction(
-//		client.Opts(),
-//		mt.To,
-//		mt.Value,
-//		mt.CallData,
-//		uint8(mt.Operation),
-//		mt.SafeTxGas,
-//		txhash,
-//		vs,
-//		rs,
-//		ss,
-//	)
-//
-//	client.UnlockOpts()
-//
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	t.Log("txHash", tx.Hash())
-//}
+func TestProxyTransferErc20(t *testing.T) {
+	password := "123456"
+	os.Setenv(keystore.EnvPassword, password)
+
+	owners := []common.Address{common.HexToAddress("0xBd39f5936969828eD9315220659cD11129071814"), common.HexToAddress("0xBca9567A9e8D5F6F58C419d32aF6190F74C880e6")}
+	keys := make([]*secp256k1.Keypair, 0)
+
+	for _, own := range owners {
+		kpI, err := keystore.KeypairFromAddress(own.Hex(), keystore.EthChain, keystorePath, false)
+		if err != nil {
+			panic(err)
+		}
+		kp, _ := kpI.(*secp256k1.Keypair)
+
+		keys = append(keys, kp)
+	}
+
+	client := NewClient(goerliEndPoint, keys[0], testLogger, big.NewInt(0), big.NewInt(0))
+	err := client.Connect()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cd, _ := maticTokenAbi.Pack("transfer", receiver1, big.NewInt(0).Mul(big.NewInt(10000000000000000), big.NewInt(10)))
+	mt := &ethmodel.MultiTransaction{
+		To:        goerliRFis,
+		Value:     big.NewInt(0),
+		CallData:  cd,
+		Operation: ethmodel.Call,
+		SafeTxGas: big.NewInt(100000),
+		TotalGas: big.NewInt(100000),
+	}
+	txhash := common.HexToHash("0x2ed668ca5c97408167f046131a37b4ef10ccbd621dabf920eefddaa62fe77e1b")
+	msg := mt.MessageToSign(txhash, goerliMultisigProxy)
+	t.Log("msg", hexutil.Encode(msg[:]))
+
+	multi, err := Multisig.NewMultisig(goerliMultisigProxy, client.Client())
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = multi
+
+	sigs := make([][]byte, 0)
+	for _, key := range keys {
+		signature, err := crypto.Sign(msg[:], key.PrivateKey())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		sigs = append(sigs, signature)
+	}
+
+	vs, rs, ss := utils.DecomposeSignature(sigs)
+	err = client.LockAndUpdateOpts(mt.TotalGas)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//tx, err := multi.ExecTransaction(
+	//	client.Opts(),
+	//	mt.To,
+	//	mt.Value,
+	//	mt.CallData,
+	//	uint8(mt.Operation),
+	//	mt.SafeTxGas,
+	//	txhash,
+	//	vs,
+	//	rs,
+	//	ss,
+	//)
+	_, _, _ = vs, rs, ss
+
+	client.UnlockOpts()
+
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+
+	//t.Log("txHash", tx.Hash())
+}
